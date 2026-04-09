4a4bbc93094d4d200bc4a54205e88d375896c2b0
shiven16 <shivenahuja2004@gmail.com>
2026-04-09 12:00:00 +0530

    feat: add custom string library (mystring.h + string.c)

---
+++ include/mystring.h
@@ -0,0 +1,8 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+int  my_strlen(const char *s);
+int  my_strcmp(const char *a, const char *b);
+void my_strcpy(char *dest, const char *src);
+
+#endif
---
+++ src/string.c
@@ -0,0 +1,25 @@
+#include "mystring.h"
+
+int my_strlen(const char *s) {
+    int len = 0;
+    while (s[len] != '\0') len++;
+    return len;
+}
+
+int my_strcmp(const char *a, const char *b) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] != b[i]) return a[i] - b[i];
+        i++;
+    }
+    return a[i] - b[i];
+}
+
+void my_strcpy(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
