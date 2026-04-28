88c9ee62f66851c93a0f6026ef25b721ef9fd845
shiven16 <shivenahuja2004@gmail.com>
2026-04-28 07:30:00 +0530

    Clean up unused getchar functions from keyboard.c and keyboard.h

--- include/keyboard.h
+++ include/keyboard.h
@@ -4,14 +4,5 @@
 void keyboard_enable_raw(void);
 void keyboard_disable_raw(void);
 int keyboard_keypressed(void);
-/* Reads one line into buf (max len-1 chars), returns number of chars read.
-   Space is ignored if no chars have been typed in the current word. */
-int keyboard_readline(char *buf, int len);
-
-/* Same as keyboard_readline but:
-   - records start time on the FIRST keypress via gettimeofday
-   - records end time when the line is complete
-   - writes elapsed microseconds into *elapsed_us */
-int keyboard_readline_timed(char *buf, int len, long long *elapsed_us);
 
 #endif
